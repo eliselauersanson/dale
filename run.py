db487c95c912757383481ed12e1acde494f6647c
elise <elisesanson@gmail.com>
2023-05-10 15:49:02 -0300

    :sparkles: fix

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ while True:
     x = random.randint(0, len(lista) - 1)
     print(f"O princípio do dia é:")
     sleep(1)
-    print(f"\n       {lista[x]}")
+    print(f"\n       {lista[x]}\n")
     sleep(1.5)
     resp = str(input("Bora aplicar este princípio no seu dia!?   [Y/N]"))
     if resp.upper()[0] != "Y":
-        print('\n Poxa, vamos tentar outro princípio então!')
+        print('\n Poxa, vamos tentar outro princípio então!\n')
         continue
     sleep(1)
     print("\nQue bom! E não se esqueça...")
